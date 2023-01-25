1e3f294bc386d5cb9176fba9fad795de0e79f491
Jaime <jsp@gmail.com>
2023-01-25 20:26:13 -0300

    asf

--- _maps/maps.js
+++ _maps/maps.js
@@ -5,7 +5,3 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-document.getElementById('select-location').addEventListener('change',function(e){
-  let coords = e.target.value.split(",");
-  map.flyTo(coords,13);
-})
