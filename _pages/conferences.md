16dc97800f9f6f9b0ebcdd437af3d17b980addd5
Jaime <jsp@gmail.com>
2023-02-03 13:43:38 +0100

    asd

--- _pages/conferences.md
+++ _pages/conferences.md
@@ -68,7 +68,7 @@ map.addLayer(markers);
 
 L.geoJSON(locations, {
   onEachFeature: function (feature, layer) {
-    layer.bindPopup('<h2 style="color:black;">'+feature.properties.Name+'</h1><p>name: '+feature.properties.Name+'</p>');
+    layer.bindPopup('<h2 style="color:black;">'+feature.properties.Conference+'</h2><p style="color:black;">name: '+feature.properties.Location+'</p>');
   }
 }).addTo(map);
 
--- assets/locations.js
+++ assets/locations.js
@@ -1,31 +1,229 @@
 var locations = {
-    "type": "FeatureCollection",
-    "features": [
-      {
-        "type": "Feature",
-        "properties": {
-          "Name": "Santiago"
-        },
-        "geometry": {
-          "coordinates": [
-            -70.64246200018779,
-            -33.445595576465244
-          ],
-          "type": "Point"
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "Name": "Sydney"
-        },
-        "geometry": {
-          "coordinates": [
-            151.2087946571247,
-            -33.870215221417226
-          ],
-          "type": "Point"
-        }
-      }
-    ]
-  }
+  "type": "FeatureCollection",
+  "features": [
+ {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ -70.641320,-33.441295 ]
+   },
+   "properties": {
+   "Conference":"IATBR",
+   "Location":"Santiago, Chile",
+   "Date":"December,2022",
+   "Title":"Who is ready to live a car-independent lifestyle? A latent class cluster analysis of attitudes towards car ownership and usage.",
+   "Link":"https://iatbr.weebly.com/2022-home.html"
+   }
+ },
+ {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ -79.405157,43.665148 ]
+   },
+   "properties": {
+   "Conference":"NECTAR",
+   "Location":"Toronto, Canada",
+   "Date":"July, 2022",
+   "Title":"Who is ready to live a car-independent lifestyle? A latent class cluster analysis of attitudes towards car ownership and usage.",
+   "Link":"https://nectar2022.artsci.utoronto.ca/"
+   }
+ },
+ {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ -71.605633,-33.044836 ]
+   },
+   "properties": {
+   "Conference":"CCHIT",
+   "Location":"Chile (Online)",
+   "Date":"October, 2021",
+   "Title":"A discrete-event public transportation simulation model to evaluate the impacts of social distancing and travel demand.",
+   "Link":"https://cchit.cl/"
+   }
+ },
+ {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ 18.068581,59.329323 ]
+   },
+   "properties": {
+   "Conference":"INSTR",
+   "Location":"Stockholm, Sweden (Online)",
+   "Date":"June, 2021",
+   "Title":"Travel preferences of public transport users under uneven headways",
+   "Link":"https://www.instr2021.conf.kth.se/"
+   }
+ },
+ {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ -70.614591,-33.497116 ]
+   },
+   "properties": {
+   "Conference":"CCHIT",
+   "Location":"Santiago, Chile",
+   "Date":"October, 2019",
+   "Title":"Travel preferences of public transport users under uneven headways <br/> What factors determine the variability of the level of service experienced by transit users?",
+   "Link":"https://cchit.ing.uc.cl/"
+   }
+ },
+ {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ 2.352222,48.856614 ]
+   },
+   "properties": {
+   "Conference":"TransitData",
+   "Location":"Paris, France",
+   "Date":"July, 2019",
+   "Title":"Public transport travel time reliability across modes and space",
+   "Link":"https://wiki.lafabriquedesmobilites.fr/wiki/TRANSITDATA_2019_minutes"
+   }
+ },
+ {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ -77.023017,38.904812 ]
+   },
+   "properties": {
+   "Conference":"TRB",
+   "Location":"Washington D.C., U.S.A",
+   "Date":"January, 2019",
+   "Title":"Lessons and evaluation of a headway control experiment in Washington D.C.",
+   "Link":"https://www.trb.org/AnnualMeeting/AnnualMeeting.aspx"
+   }
+ },
+ {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ -75.590553,6.230833 ]
+   },
+   "properties": {
+   "Conference":"PANAM",
+   "Location":"Medell�n, Colombia",
+   "Date":"September, 2018",
+   "Title":"The underlying effect of public transport reliability over users' satisfaction <br/> What factors determine the variability of the level of service experienced by transit users?",
+   "Link":"https://medellin.unal.edu.co/eventos/panam2018/index.php/en/"
+   }
+ },
+ {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ 153.021072,-27.469800 ]
+   },
+   "properties": {
+   "Conference":"CASPT",
+   "Location":"Brisbane, Australia",
+   "Date":"July, 2018",
+   "Title":"The underlying effect of public transport reliability over users' satisfaction <br/> What factors determine the variability of the level of service experienced by transit users?",
+   "Link":"http://www.caspt.org/"
+   }
+ },
+ {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ -119.698190,34.420831 ]
+   },
+   "properties": {
+   "Conference":"IATBR",
+   "Location":"Santa Barbara, U.S.A.",
+   "Date":"July, 2018",
+   "Title":"The underlying effect of public transport reliability over users' satisfaction",
+   "Link":"https://iatbr.weebly.com/iatbr-conferencesbooks.html"
+   }
+ },
+ {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ 151.209290,-33.868820 ]
+   },
+   "properties": {
+   "Conference":"INSTR",
+   "Location":"Sydney, Australia",
+   "Date":"January, 2018",
+   "Title":"The underlying effect of public transport reliability over users' satisfaction <br/> Closing the gap between perceived and objective accessibility measures: A new approach to measuring public transport accessibility considering comfort, transfers and reliability perception",
+   "Link":"https://instr.org/"
+   }
+ },
+ {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ -77.022602,38.907192 ]
+   },
+   "properties": {
+   "Conference":"TRB",
+   "Location":"Washington D.C., U.S.A",
+   "Date":"January, 2018",
+   "Title":"Public transport travel time reliability across modes and space",
+   "Link":"https://www.trb.org/AnnualMeeting/AnnualMeeting.aspx"
+   }
+ },
+ {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ -71.274405,-29.911284 ]
+   },
+   "properties": {
+   "Conference":"CCHIT",
+   "Location":"La Serena, Chile",
+   "Date":"October, 2017",
+   "Title":"Characterizing the differences on public transport travel time reliability between travellers and operators",
+   "Link":"https://sochitran.cl/2017/10/24/18-congreso-chileno-de-ingenieria-de-transporte-aristo-cityplanning-cipres-la-serena/"
+   }
+ },
+ {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ 34.989571,32.794044 ]
+   },
+   "properties": {
+   "Conference":"hEART",
+   "Location":"Haifa, Israel",
+   "Date":"September, 2017",
+   "Title":"What factors determine the variability of the level of service experienced by transit users?",
+   "Link":"https://heart2017.net.technion.ac.il"
+   }
+ },
+ {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ 19.040236,47.497913 ]
+   },
+   "properties": {
+   "Conference":"EWGT",
+   "Location":"Budapest, Hungary",
+   "Date":"September, 2017",
+   "Title":"Characterizing the differences on public transport travel time reliability between travellers and operators",
+   "Link":"http://ewgt2017.bme.hu/"
+   }
+ },
+ {
+   "type": "Feature",
+   "geometry": {
+      "type": "Point",
+      "coordinates":  [ -70.663353,-33.456115 ]
+   },
+   "properties": {
+   "Conference":"TransitData",
+   "Location":"Santiago, Chile",
+   "Date":"April, 2017",
+   "Title":"Characterizing the differences on public transport travel time reliability between travellers and operators",
+   "Link":"https://transitdata.cl/2/default/inicio"
+   }
+ }
+]
+}
