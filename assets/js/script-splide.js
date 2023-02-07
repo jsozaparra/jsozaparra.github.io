555384fe2f07a5d1312be35ab0fc944044210789
Jaime <jsp@gmail.com>
2023-02-07 15:48:20 +0100

    as

--- assets/js/script-splide.js
+++ assets/js/script-splide.js
@@ -1,8 +1,9 @@
 new Splide( '#card-carousel', {
-  perPage    : 3,
+  perPage    : 1,
   gap:  '3rem',
   type   : 'loop',
   autoplay : true,
+  interval: 3000,
   breakpoints: {
     640: {
       perPage: 1,
