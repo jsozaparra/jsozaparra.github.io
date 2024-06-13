d22e8988abbf04b7a95477ea89d6707d1236dc20
jsozaparra <jaime.sozaparra@gmail.com>
2024-06-13 17:58:09 +0200

    Update conferences 2023 2024 [ci skip]

--- _maps/maps.js
+++
@@ -1,7 +0,0 @@
-let map = L.map('map').setView([4.639386,-74.082412],6)
-
-//Agregar tilelAyer mapa base desde openstreetmap
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
